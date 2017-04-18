8dbcc42e60fb0ea1beeacf5386c0a80efa4f9326
Russ Olsen <russ@russolsen.com>
2017-04-18 11:57:09 -0400

    Switch the polarity of the deep flag to shallow.

--- src/fern/config.clj
+++ src/fern/config.clj
@@ -3,15 +3,15 @@
 
 (declare evaluate*)
 
-(defn- eval-f [cfg deep depth]
-  (fn [x] (evaluate* x cfg deep (inc depth))))
+(defn- eval-f [cfg shallow depth]
+  (fn [x] (evaluate* x cfg shallow (inc depth))))
 
-(defn evaluate-dispatch-f [x cfg deep depth]
+(defn evaluate-dispatch-f [x cfg shallow depth]
   (when (> depth 100)
     (throw 
       (ex-info
         (str "Runnaway evaluation recursion while evaluating [" x "].")
-        {:cfg cfg :deep deep :depth depth})))
+        {:cfg cfg :shallow shallow :depth depth})))
 
   (cond
     (record? x) (class x)
@@ -30,32 +30,32 @@
 (defmethod evaluate* :string [x _ _ _] x)
 (defmethod evaluate* :keyword [x _ _ _] x)
 
-(defmethod evaluate* :symbol [x cfg deep depth]
-  (if-not deep
+(defmethod evaluate* :symbol [x cfg shallow depth]
+  (if shallow
     x
-    (evaluate* (cfg x) cfg deep (inc depth))))
+    (evaluate* (cfg x) cfg shallow (inc depth))))
 
-(defmethod evaluate* :vector [x cfg deep depth]
-  (if-not deep
+(defmethod evaluate* :vector [x cfg shallow depth]
+  (if shallow
     x
-    (mapv (eval-f cfg deep depth) x)))
+    (mapv (eval-f cfg shallow depth) x)))
 
-(defmethod evaluate* :map [x cfg deep depth]
-  (if-not deep
+(defmethod evaluate* :map [x cfg shallow depth]
+  (if shallow
     x
-    (zipmap (map (eval-f cfg deep depth) (keys x))
-            (map (eval-f cfg deep depth) (vals x)))))
+    (zipmap (map (eval-f cfg shallow depth) (keys x))
+            (map (eval-f cfg shallow depth) (vals x)))))
 
-(defmethod evaluate* :list [x cfg deep depth]
+(defmethod evaluate* :list [x cfg shallow depth]
   (cond
     (= (first x) 'quote) (second x)
-    deep                 (map (eval-f cfg deep depth) x)
+    shallow              (map (eval-f cfg shallow depth) x)
     :default             x))
 
 (defmethod evaluate* :default [x _ _ _] x)
     
 
 (defn evaluate 
-  ([x cfg] (evaluate* x cfg true 0))
-  ([x cfg deep] (evaluate* x cfg deep 0)))
+  ([x cfg] (evaluate* x cfg false 0))
+  ([x cfg shallow] (evaluate* x cfg shallow 0)))
 
