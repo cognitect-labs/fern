82b505399d3db1349961a7c630c20d98e44b0113
Russ Olsen <russ@russolsen.com>
2017-04-18 10:40:15 -0400

    Add prototype flexible map based config.

---
+++ src/fern/config.clj
@@ -0,0 +1,64 @@
+(ns
+  "Flexible configuration in a Clojure map."
+  fern.config
+  (:refer-clojure :exclude [read-string])
+  (:require [clojure.string :as str]))
+
+
+(declare evaluate*)
+
+(defn- eval-f [cfg deep depth]
+  (fn [x] (evaluate* x cfg deep (inc depth))))
+
+(defn evaluate-dispatch-f [x cfg deep depth]
+  (when (> depth 100)
+    (throw 
+      (ex-info
+        (str "Runnaway evaluation recursion while evaluating [" x "].")
+        {:cfg cfg :deep deep :depth depth})))
+
+  (cond
+    (symbol? x) :symbol
+    (map? x) :map
+    (keyword? x) :keyword
+    (vector? x) :vector
+    (number? x) :number
+    (string? x) :string
+    (list? x) :list
+    :default (class x)))
+
+(defmulti evaluate* evaluate-dispatch-f)
+
+(defmethod evaluate* :number [x _ _ _] x)
+(defmethod evaluate* :string [x _ _ _] x)
+(defmethod evaluate* :keyword [x _ _ _] x)
+
+(defmethod evaluate* :symbol [x cfg deep depth]
+  (if-not deep
+    x
+    (evaluate* (cfg x) cfg deep (inc depth))))
+
+(defmethod evaluate* :vector [x cfg deep depth]
+  (if-not deep
+    x
+    (mapv (eval-f cfg deep depth) x)))
+
+(defmethod evaluate* :map [x cfg deep depth]
+  (if-not deep
+    x
+    (zipmap (map (eval-f cfg deep depth) (keys x))
+            (map (eval-f cfg deep depth) (vals x)))))
+
+(defmethod evaluate* :list [x cfg deep depth]
+  (cond
+    (= (first x) 'quote) (second x)
+    deep                 (map (eval-f cfg deep depth) x)
+    :default             x))
+
+(defmethod evaluate* :default [x _ _ _] x)
+    
+
+(defn evaluate 
+  ([x cfg] (evaluate* x cfg true 0))
+  ([x cfg deep] (evaluate* x cfg deep 0)))
+
