860047e315f596d113357bd057f72204bc5710d3
Michael Nygard <mtnygard@cognitect.com>
2017-09-30 12:48:33 -0500

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cognitect/fern "0.1.3"
+(defproject com.cognitect/fern "0.1.4-SNAPSHOT"
   :description  "Plain but useful language for data"
   :url          "https://github.com/cognitect-labs/fern"
   :license      {:name "Eclipse Public License"
