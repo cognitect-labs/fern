a0f5ca5a3f0133291dd70adb34761e76613ac3a8
Michael Nygard <mtnygard@gmail.com>
2018-09-29 16:13:09 -0500

    Workaround needed on JDK 11

--- project.clj
+++ project.clj
@@ -5,7 +5,11 @@
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15" :scope "provided"]
                  [org.clojure/tools.reader "1.0.0-beta4"]
-                 [fipp "0.6.8"]
+                 [fipp "0.6.12" :exclusions [org.clojure/core.rrb-vector]]
+                 ;; Workaround for
+                 ;; https://dev.clojure.org/jira/browse/CRRBV-18 until
+                 ;; next release of fipp
+                 [org.clojure/core.rrb-vector "0.0.12"]
                  [mvxcvi/puget "1.0.1"]
                  [jline/jline "2.14.2"]]
   :profiles     {:dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]]
