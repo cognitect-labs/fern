464cb4792c1fccd6e6090125261c7ab6d106031c
Davide Taviani <helios89@gmail.com>
2018-12-16 10:19:10 +0100

    Bump fipp version

--- project.clj
+++ project.clj
@@ -5,11 +5,7 @@
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/tools.reader "1.3.0"]
-                 [fipp "0.6.12" :exclusions [org.clojure/core.rrb-vector]]
-                 ;; Workaround for
-                 ;; https://dev.clojure.org/jira/browse/CRRBV-18 until
-                 ;; next release of fipp
-                 [org.clojure/core.rrb-vector "0.0.12"]
+                 [fipp "0.6.14"]
                  [mvxcvi/puget "1.0.1"]
                  [jline/jline "2.14.2"]]
   :profiles     {:dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]]
